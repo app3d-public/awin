44ce2b9d2b647b0db05eda092da2f54748d97a05
wj <wusikijeronii@gmail.com>
2024-07-19 20:04:56 +0300

    Separate namespace

--- include/window/types.hpp
+++ include/window/types.hpp
@@ -8,167 +8,167 @@
 #define KEY_MOD_START_INDEX 106
 
 // Keys
-namespace io
-{
-    enum class Key : i16
-    {
-        kUnknown,
-        kSpace,
-        kApostroph,
-        kComma,
-        kMinus,
-        kPeriod,
-        kSlash,
-        k0,
-        k1,
-        k2,
-        k3,
-        k4,
-        k5,
-        k6,
-        k7,
-        k8,
-        k9,
-        kSemicolon,
-        kEqual,
-        kA,
-        kB,
-        kC,
-        kD,
-        kE,
-        kF,
-        kG,
-        kH,
-        kI,
-        kJ,
-        kK,
-        kL,
-        kM,
-        kN,
-        kO,
-        kP,
-        kQ,
-        kR,
-        kS,
-        kT,
-        kU,
-        kV,
-        kW,
-        kX,
-        kY,
-        kZ,
-        kLeftBrace,
-        kBackslash,
-        kRightBrace,
-        kGraveAccent,
-        kEscape,
-        kEnter,
-        kTab,
-        kBackspace,
-        kInsert,
-        kDelete,
-        kRight,
-        kLeft,
-        kDown,
-        kUp,
-        kPageUp,
-        kPageDown,
-        kHome,
-        kEnd,
-        kPrintScreen,
-        kPause,
-        kF1,
-        kF2,
-        kF3,
-        kF4,
-        kF5,
-        kF6,
-        kF7,
-        kF8,
-        kF9,
-        kF10,
-        kF11,
-        kF12,
-        kF13,
-        kF14,
-        kF15,
-        kF16,
-        kF17,
-        kF18,
-        kF19,
-        kF20,
-        kF21,
-        kF22,
-        kF23,
-        kF24,
-        kKP0,
-        kKP1,
-        kKP2,
-        kKP3,
-        kKP4,
-        kKP5,
-        kKP6,
-        kKP7,
-        kKP8,
-        kKP9,
-        kKPDecimal,
-        kKPDivide,
-        kKPMultiply,
-        kKPSubtract,
-        kKPAdd,
-        kKPEnter,
-        kKPEqual,
-        kCapsLock,
-        kScrollLock,
-        kNumLock,
-        kLeftShift,
-        kLeftControl,
-        kLeftAlt,
-        kLeftSuper,
-        kRightShift,
-        kRightControl,
-        kRightAlt,
-        kRightSuper,
-        kMenu,
-        kLast = kMenu
-    };
-
-    inline constexpr i16 operator+(Key k) { return static_cast<i16>(k); }
-    inline constexpr i16 operator+(Key lhs, i16 rhs) { return static_cast<i16>(lhs) + rhs; }
-
-    enum class MouseKey
-    {
-        unknown = -1,
-        left = 0,
-        right = 1,
-        middle = 2
-    };
-
-    inline constexpr i16 operator+(MouseKey k) { return static_cast<i16>(k); }
-    inline constexpr i16 operator+(MouseKey lhs, i16 rhs) { return static_cast<i16>(lhs) + rhs; }
-
-    enum class KeyPressState : i8
-    {
-        release,
-        press,
-        repeat
-    };
-
-    enum class KeyModeBits : i8
-    {
-        shift = 0x0001,
-        control = 0x0002,
-        alt = 0x0004,
-        super = 0x0008,
-        capsLock = 0x0010,
-        numLock = 0x0020
-    };
-
-    using KeyMode = Flags<KeyModeBits>;
-} // namespace io
-
 namespace window
 {
+    namespace io
+    {
+        enum class Key : i16
+        {
+            kUnknown,
+            kSpace,
+            kApostroph,
+            kComma,
+            kMinus,
+            kPeriod,
+            kSlash,
+            k0,
+            k1,
+            k2,
+            k3,
+            k4,
+            k5,
+            k6,
+            k7,
+            k8,
+            k9,
+            kSemicolon,
+            kEqual,
+            kA,
+            kB,
+            kC,
+            kD,
+            kE,
+            kF,
+            kG,
+            kH,
+            kI,
+            kJ,
+            kK,
+            kL,
+            kM,
+            kN,
+            kO,
+            kP,
+            kQ,
+            kR,
+            kS,
+            kT,
+            kU,
+            kV,
+            kW,
+            kX,
+            kY,
+            kZ,
+            kLeftBrace,
+            kBackslash,
+            kRightBrace,
+            kGraveAccent,
+            kEscape,
+            kEnter,
+            kTab,
+            kBackspace,
+            kInsert,
+            kDelete,
+            kRight,
+            kLeft,
+            kDown,
+            kUp,
+            kPageUp,
+            kPageDown,
+            kHome,
+            kEnd,
+            kPrintScreen,
+            kPause,
+            kF1,
+            kF2,
+            kF3,
+            kF4,
+            kF5,
+            kF6,
+            kF7,
+            kF8,
+            kF9,
+            kF10,
+            kF11,
+            kF12,
+            kF13,
+            kF14,
+            kF15,
+            kF16,
+            kF17,
+            kF18,
+            kF19,
+            kF20,
+            kF21,
+            kF22,
+            kF23,
+            kF24,
+            kKP0,
+            kKP1,
+            kKP2,
+            kKP3,
+            kKP4,
+            kKP5,
+            kKP6,
+            kKP7,
+            kKP8,
+            kKP9,
+            kKPDecimal,
+            kKPDivide,
+            kKPMultiply,
+            kKPSubtract,
+            kKPAdd,
+            kKPEnter,
+            kKPEqual,
+            kCapsLock,
+            kScrollLock,
+            kNumLock,
+            kLeftShift,
+            kLeftControl,
+            kLeftAlt,
+            kLeftSuper,
+            kRightShift,
+            kRightControl,
+            kRightAlt,
+            kRightSuper,
+            kMenu,
+            kLast = kMenu
+        };
+
+        inline constexpr i16 operator+(Key k) { return static_cast<i16>(k); }
+        inline constexpr i16 operator+(Key lhs, i16 rhs) { return static_cast<i16>(lhs) + rhs; }
+
+        enum class MouseKey
+        {
+            unknown = -1,
+            left = 0,
+            right = 1,
+            middle = 2
+        };
+
+        inline constexpr i16 operator+(MouseKey k) { return static_cast<i16>(k); }
+        inline constexpr i16 operator+(MouseKey lhs, i16 rhs) { return static_cast<i16>(lhs) + rhs; }
+
+        enum class KeyPressState : i8
+        {
+            release,
+            press,
+            repeat
+        };
+
+        enum class KeyModeBits : i8
+        {
+            shift = 0x0001,
+            control = 0x0002,
+            alt = 0x0004,
+            super = 0x0008,
+            capsLock = 0x0010,
+            numLock = 0x0020
+        };
+
+        using KeyMode = Flags<KeyModeBits>;
+    } // namespace io
+
     // Basic information about a monitor/display.
     struct MonitorInfo
     {
@@ -259,12 +259,12 @@ struct FlagTraits<window::CreationFlagsBits>
 };
 
 template <>
-struct FlagTraits<io::KeyModeBits>
+struct FlagTraits<window::io::KeyModeBits>
 {
     static constexpr bool isBitmask = true;
-    static constexpr io::KeyMode allFlags = io::KeyModeBits::shift | io::KeyModeBits::control | io::KeyModeBits::alt |
-                                            io::KeyModeBits::super | io::KeyModeBits::capsLock |
-                                            io::KeyModeBits::numLock;
+    static constexpr window::io::KeyMode allFlags =
+        window::io::KeyModeBits::shift | window::io::KeyModeBits::control | window::io::KeyModeBits::alt |
+        window::io::KeyModeBits::super | window::io::KeyModeBits::capsLock | window::io::KeyModeBits::numLock;
 };
 
 #endif
